3dec8f0d88d39f30dc9dc7576abf63564ed40a48
Nikolay Lapshin <nlapshin1989@gmail.com>
2023-08-17 21:08:01 +0300

    Fixed commit

--- src/index.spec.js
+++ src/index.spec.js
@@ -17,7 +17,7 @@ describe('GET /hello/:name', () => {
       .get('/hello/nik')
       .set('Accept', 'application/json');
 
-    expect(res.body).toEqual({ msg: 'Hello nik from express version 4' })
+    expect(res.body).toEqual({ msg: 'Hello my nik from express version 4' })
   });
 
   test('Should return welcome string 2', async () => {
@@ -25,7 +25,7 @@ describe('GET /hello/:name', () => {
       .get('/hello/bad-user')
       .set('Accept', 'application/json');
 
-    expect(res.body).toEqual({ msg: 'Hello bad-user from express version 4' })
+    expect(res.body).toEqual({ msg: 'Hello my bad-user from express version 4' })
   });
 
   test('Should return welcome string 3', async () => {
@@ -33,6 +33,6 @@ describe('GET /hello/:name', () => {
       .get('/hello/100200')
       .set('Accept', 'application/json');
 
-    expect(res.body).toEqual({ msg: 'Hello 100200 from express version 4' })
+    expect(res.body).toEqual({ msg: 'Hello my 100200 from express version 4' })
   });
 });
